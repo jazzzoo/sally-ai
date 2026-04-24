915a289e42284a5ffdd27eb1750f504a9c08ac9a
leetech <orangesbdj@gmail.com>
2026-04-24 09:29:25 +0900

    fix: browser tab title - disable NavigationContainer documentTitle

--- frontend/App.js
+++ frontend/App.js
@@ -111,6 +111,7 @@ export default function App() {
 
           <NavigationContainer
             ref={navRef}
+            documentTitle={{ enabled: false }}
             linking={linking}
             onStateChange={(state) => {
               const route = state?.routes?.[state.index];
